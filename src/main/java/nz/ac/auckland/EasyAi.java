1144cb3ce00d73a349795d2a146043020734a764
Puthikunkim <jerryputhikunkim@gmail.com>
2024-05-03 19:17:21 +1200

    Starting to implement task 2 by creating factory files

---
+++ src/main/java/nz/ac/auckland/EasyAi.java
@@ -0,0 +1,12 @@
+package nz.ac.auckland;
+
+import nz.ac.auckland.se281.Ai;
+
+public class EasyAi implements Ai {
+
+  @Override
+  public int getFingers() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'getFingers'");
+  }
+}
